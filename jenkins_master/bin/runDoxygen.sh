7f2e1f52ab4f4e0d9130e7cb4712a4717fcffb9e
Thorsten Krug <email@donkeymedia.eu>
2017-12-07 16:06:21 -0500

    Refactor branch navi.

--- jenkins_master/bin/runDoxygen.sh
+++ jenkins_master/bin/runDoxygen.sh
@@ -4,29 +4,34 @@ echo "# BRANCHNAME"
 BRANCHNAME=$(/bin/sed 's/^origin\///' <<< "$GIT_BRANCH")
 #echo ${BRANCHNAME}
 
-echo "# WORKDIR"
-WORKDIR="$WORKSPACE/doc/$BRANCHNAME"
-#echo ${WORKDIR}
+echo "# DOCDIR"
+DOCDIR="$WORKSPACE/doc/$BRANCHNAME"
+#echo ${DOCDIR}
 
 echo "# DESTINATION"
 DESTINATION="/apidocs/$BRANCHNAME"
 #echo ${DESTINATION}
 
-if [ -d "$WORKDIR" ]; then
-    rm -rf "$WORKDIR"
+if [ -d "$DOCDIR" ]; then
+    rm -rf "$DOCDIR"
 fi
-mkdir -p "$WORKDIR"
+mkdir -p "$DOCDIR"
 
+# Prepare template, that SSI of branch-navi works everywhere.
+sed -i "s/<!--#include virtual=\"navi.html\" -->/<!--#include virtual=\"\/$BRANCHNAME\/navi.html\" -->/g" "$WORKSPACE/resources/doxygen-assets/header.html"
 
 echo "### DOXYGEN CONFIG ###"
-(cat "$WORKSPACE/Doxyfile" ; echo "OUTPUT_DIRECTORY = \"$(printf %q "$WORKDIR")\"")|cat
+(cat "$WORKSPACE/Doxyfile" ; echo "OUTPUT_DIRECTORY = \"$(printf %q "$DOCDIR")\"")|cat
 echo ""
 
 echo "### GENERATING API DOC ###"
-(cat "$WORKSPACE/Doxyfile" ; echo "OUTPUT_DIRECTORY = \"$(printf %q "$WORKDIR")\"")|doxygen -
+(cat "$WORKSPACE/Doxyfile" ; echo "OUTPUT_DIRECTORY = \"$(printf %q "$DOCDIR")\"")|doxygen -
 
 if [ -d "$DESTINATION" ]; then
 	echo "Clean up $DESTINATION"
     rm -rf "$DESTINATION"
 fi
-cp -R "$WORKDIR" "$DESTINATION"
+cp -R "$DOCDIR" "$DESTINATION"
+
+# Generate a SELECT-navi from all directories in /apidocs.
+/usr/local/bin/makeApiNav.sh /apidocs "$DESTINATION/navi.html" "$BRANCHNAME"
